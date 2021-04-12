c31f515cc50b80e4f655b6c7fb9ca16fb4e3a43c
Alexandre LAM <alexandrelam@outlook.com>
2021-04-12 13:51:12 +0200

    uct score in node + some getters and setters

--- src/tictactoecodingame/Node.java
+++ src/tictactoecodingame/Node.java
@@ -3,43 +3,60 @@ package tictactoecodingame;
 import java.util.ArrayList;
 
 public class Node {
-    private int nbVisite; 
+    private int nbVisite;
     private int scoreVictoire;
+    private double uctScore;
     private Coup coup;
     private Node parent;
     private ArrayList<Node> childArray;
 
     public Node() {
+        this.uctScore = 0;
         this.childArray = new ArrayList<>();
     }
 
-    public Node(Coup coup, Node parent){
+    public Node(Coup coup, Node parent) {
+        this.uctScore = 0;
         this.coup = coup;
         this.parent = parent;
         this.childArray = new ArrayList<>();
-    }
-
-    public State getState() {
-        return this.state;
     }
 
     public Node getParent() {
         return this.parent;
     }
 
-    public int getNbVisite(){
+    public int getNbVisite() {
         return this.nbVisite;
     }
 
-    public int getScoreVictoire(){
+    public int getScoreVictoire() {
         return this.scoreVictoire;
+    }
+
+    public Coup getCoup(){
+        return this.coup;
+    }
+
+    public double getUCT(){
+        return this.uctScore;
     }
 
     public ArrayList<Node> getChildArray() {
         return this.childArray;
     }
 
+    public void setCoup(Coup coup){
+        this.coup = coup;
+    }
+
     public void setChildArray(ArrayList<Node> childArray) {
         this.childArray = childArray;
+    }
+
+    public void updateUctScore(double C){
+        double exploitation =  this.scoreVictoire / this.nbVisite;
+        double exploration = C * Math.sqrt(Math.log(this.parent.getNbVisite() / this.nbVisite));
+        this.uctScore = exploitation + exploration;
     }
 }
