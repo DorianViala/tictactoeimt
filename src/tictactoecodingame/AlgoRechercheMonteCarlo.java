69960bf1176b5d19fcb591e4fc88d08e2d9983ef
RomainGuarinoni <romain.guar91@gmail.com>
2021-04-12 17:25:33 +0200

    remove useless import

--- src/tictactoecodingame/AlgoRechercheMonteCarlo.java
+++ src/tictactoecodingame/AlgoRechercheMonteCarlo.java
@@ -2,7 +2,6 @@ package tictactoecodingame;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.Random;
 
 public class AlgoRechercheMonteCarlo extends AlgoRecherche {
